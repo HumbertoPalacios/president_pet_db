c2e452648ce85a90fde3f491872a05a156dfdfad
Humberto Palacios <palacioshumberto35@yahoo.com>
2024-01-27 16:03:22 -0800

    controllers

---
+++ petController.js
@@ -0,0 +1,138 @@
+import express from "express"; // Import express
+import pg from "pg"; // Import pg
+import fs from "fs"; // Import fs
+
+const fsPromise = fs.promises; // Assign fs.promises to fsPromise for promise-based file operations
+
+const PORT = 8001; // Assign 8001 to be our port
+
+const app = express(); // Invoke express and store in in the app const
+
+app.use(express.json()); // Invoke middleware to parse incoming JSON requests
+
+const pool = new pg.Pool({ // Create a collection of database connections (new pool instances) and assign it to the pool const 
+    host: 'localhost',
+    port: 6432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'president_pet_db',
+});
+
+// GET method that retrieves all pets from db
+app.get("/pets", (req, res) => {
+    pool.query(`SELECT * FROM pet`) // Query that selects all pet records from db
+    .then((data) => { // If query was successful
+        res.json(data.rows) // Respond with all records in JSON format
+    })
+    .catch((err) => { // If query was unsuccessful
+        console.error(err); // Log out error
+        res.sendStatus(500); // Respond 500 Internal Server Error
+    })
+});
+
+// GET method that retrieves a single pet with a specific Id 
+app.get("/pets/:id", (req, res) => {
+    const id = parseInt(req.params.id); // Parse string into into and store it in const
+    if(isNaN(id)) { // Verify if id is a number 
+        res.status(400).send("Invalid Id"); // If not, respons with a message
+        return; // And terminate the execution
+    }
+
+    pool.query(`SELECT * FROM pet WHERE petId = $1`, [id]) // Query that selects all data from a pet with a specific ID
+    .then((data) => { // If query was successful
+        if(data.rows.length === 0) { // Verify if pet with Id exists 
+            res.status(404).send("No pet with that Id found."); // If not, send message
+            return; // And terminate the execution 
+        }
+        else { // If pet with Id exists
+            res.json(data.rows[0]); // Respond with record in JSON format
+        }
+    })
+    .catch((err) => { // If query was unsuccessful
+        console.error(err); // Log out error
+        res.sendStatus(500); // Respond 500 Internal Server Error
+    })
+});
+
+// POST method the adds a pet record into db
+app.post("/pets", (req, res) => {
+    const fullName = req.body.fullName; // Store input into fullName const 
+    const species = req.body.species; // Store input into species const 
+    const presidentId = parseInt(req.body.presidentId); // Parse string into int and store it in presidentId const
+
+    if(!fullName || !species || !presidentId) { // Verify if client inputted required data
+        res.status(400).send("Missing pet information."); // If not, send message 
+        return; // And terminate execution
+    }
+
+    // Query that inserts record into table with given input
+    pool.query(`INSERT INTO pet (fullName, species, presidentId) VALUES ($1, $2, $3) RETURNING *`, [fullName, species, presidentId])
+    .then((data) => { // If query was successful
+        res.json(data.rows[0]); // Respond with created record in JSON format
+    })
+    .catch((err) => { // If query was unsuccessful 
+        console.error(err); // Log out error
+        res.sendStatus(500); // And respond with a 500 Internal Server Error
+    })
+});
+
+// PATCH method that updates certain record from a specific ID
+app.patch("/pets/:id", (req, res) => {
+    const id = parseInt(req.params.id); // Parse string into an int and store in variable 
+    if(isNaN(id)) { // Verify if input is a number
+        res.status(400).send("Invalid ID"); // If not, respond with message
+        return; // And terminate execution
+    }
+
+    // Extract the values from req.body and initialize to null if undefined
+    const fullName = req.body.fullName;
+    const species = req.body.species;
+    const presidentId = req.body.presidentId ? parseInt(req.body.presidentId) : null;
+
+     // Query that updates certain information from a record with a specific Id. However, if no update on a value, keep the value from db
+    pool.query(`UPDATE pet SET 
+    fullName = COALESCE($1, fullName),
+    species = COALESCE($2, species),
+    presidentId = COALESCE($3, presidentId)
+    WHERE petId = $4 RETURNING *`, [fullName, species, presidentId, id])
+    .then((data) => { // If query was successful,
+        if(data.rows.length === 0) { // Verify if pet with Id exists 
+            res.status(404).send("No pet with that Id found."); // If not, respond with message
+            return; // Terminate execution
+        } else { // If pet with Id does exist
+            res.json(data.rows[0]); // Respond with updated record in JSON format
+        }
+    })
+    .catch((err) => { // If query was unsuccessful
+        console.error(err); // Log out error
+        res.sendStatus(500); // And respond with 500 Internal Server Error
+    })
+})
+
+// DELETE method that deletes a record with a given Id
+app.delete("/pets/:id", (req, res) => {
+    const id = parseInt(req.params.id); // Parse string into an int and store in variable 
+    if(isNaN(id)) { // Verify is input is a number
+        res.status(400).send("Invalid ID"); // If not, respond with message
+        return; // And terminate execution 
+    }
+
+    pool.query(`DELETE FROM pet WHERE petId = $1 RETURNING *`, [id]) // Query that deletes record with a specific Id 
+    .then((data) => { // If query was successful
+        if(data.rows.length === 0) { // Verify if pet with Id exists 
+            res.status(404).send("No pet with that Id found."); // If not, respond with message
+            return; // And terminate execution
+        } else { // If pet with id exists
+            res.status(200).send(`Pet with id: ${id} has been deleted.`) // Respond with a 200 and message stating the deletion was successful
+        }
+    })
+    .catch((err) => { // If query was unsuccessful 
+        console.error(err); // Log error 
+        res.sendStatus(500); // Respond with a 500 Internal Server Error
+    })
+})
+
+// LISTEN method that listens on a given port for request
+app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`);
+});
---
+++ presidentController.js
@@ -0,0 +1,144 @@
+import express from "express"; // Import express
+import pg from "pg"; // Import pg
+import fs from "fs"; // Import fs
+
+const fsPromise = fs.promises; // Assign fs.promises to fsPromise for promise-based file operations
+
+const PORT = 8001; // Assign 8001 to be our port
+
+const app = express(); // Invoke express and store in in the app const
+
+app.use(express.json()); // Invoke middleware to parse incoming JSON requests
+
+const pool = new pg.Pool({ // Create a collection of database connections (new pool instances) and assign it to the pool const 
+    host: 'localhost',
+    port: 6432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'president_pet_db',
+})
+
+// GET method that retrieves all records from the president table 
+app.get("/presidents", (req, res) => {
+    pool.query(`SELECT * FROM president`) // Query the selects all the records from the president table
+        .then((data) => { // If query was successful
+            res.json(data.rows) // Respond with all the records in JSON format
+        })
+        .catch((err) => { // If query was unsuccessful 
+            console.error(err); // Log the error
+            res.status(500); // And respond with 500 Internal Server Error
+        })
+})
+
+// GET method that retrieves a single record from the president table given a specific ID
+app.get("/presidents/:id", (req, res) => {
+    const id = parseInt(req.params.id); // Parse string into int and store it in id const
+
+    if(isNaN(id)) { // Verify if id is a number 
+        res.status(400).send("Invalid ID"); // If not, send message
+        return; // And terminate execution
+    }
+
+    pool.query(`SELECT * FROM president WHERE presidentId = $1`, [id]) // Query that selects a single president with a specific Id
+    .then((data) => { // If query was successful
+        if (data.rows.length === 0) { // Verify if such data with ID exists
+            res.sendStatus(404); // If not, send 404 message
+            return; // And terminate execution 
+        }
+        res.json(data.rows[0]); // If it does exist, respond with selected data in JSON format
+    })
+    .catch((err) => { // If query was unsuccessful
+        console.error(err); // Log error message
+        res.sendStatus(500); // And respond with 500 Internal Servor Error message
+    })
+})
+
+// POST METHOD that adds a new record in president table 
+app.post("/presidents", (req, res) => {
+
+    const chronologicalNumber = parseInt(req.body.chronologicalNumber); // parse string into an int and store it in a const
+    const fullName = req.body.fullName; // store fullName input into a a const
+    const startYear = parseInt(req.body.startYear); // parse string into an int and store it in a const
+    const endYear = parseInt(req.body.endYear); // parse string into an int and store it in a const
+    const hadPets = Boolean(req.body.hadPets); // change string string into a boolean and store in a const 
+
+    if(!chronologicalNumber || !fullName || !startYear || !endYear || !hadPets) { // Verify if client inputted required data
+        res.status(400).send("Missing President Information"); // If not, send message 
+        return; // Terminate execution 
+    }
+
+    // Query that inserts record into table with given input
+    pool.query(`INSERT INTO president (chronologicalNumber, fullName, startYear, endYear, hadPets)
+    VALUES ($1, $2, $3, $4, $5) RETURNING *`, [chronologicalNumber, fullName, startYear, endYear, hadPets])
+    .then((data) => { // If query was successful, 
+        res.json(data.rows[0]); // Respond with created data in JSON format
+    })
+    .catch((err) => { // If query was unsuccessful
+        console.error(err); // Log error message
+        res.sendStatus(500); // Respond with 500 Internal Server Error
+    })
+})
+
+// PATCH method that updates certain record from a specific ID
+app.patch("/presidents/:id", (req, res) => {
+    const id = parseInt(req.params.id); // Parse string into an int and store in variable 
+    if (isNaN(id)) { // Verify if it is a number
+        res.status(400).send("Invalid ID"); // If not, send 400 message 
+        return; // And terminate execution 
+    }
+
+    // Extract the values from req.body and initialize to null if undefined
+    const chronologicalNumber = req.body.chronologicalNumber ? parseInt(req.body.chronologicalNumber) : null;
+    const startYear = req.body.startYear ? parseInt(req.body.startYear) : null;
+    const endYear = req.body.endYear ? parseInt(req.body.endYear) : null;
+    const fullName = req.body.fullName;
+    const hadPets = req.body.hadPets === 'true' ? true : req.body.hadPets === 'false' ? false : null; // Convert 'hadPets' correctly
+
+    // Query that updates certain information from a record with a specific Id. However, if no update on a value, keep the value from db
+    pool.query(`UPDATE president SET
+    chronologicalNumber = COALESCE($1, chronologicalNumber),
+    fullName = COALESCE($2, fullName),
+    startYear = COALESCE($3, startYear),
+    endYear = COALESCE($4, endYear),
+    hadPets = COALESCE($5, hadPets)
+    WHERE presidentId = $6 RETURNING *`,
+    [chronologicalNumber, fullName, startYear, endYear, hadPets, id])
+    .then((data) => { // If query was successful
+        if(data.rows.length === 0) { // Verify if record was updated
+            res.sendStatus(404); // If not, send 404 message
+            return; // And terminate execution 
+        }
+        res.json(data.rows[0]); // If record was updated, respond with data in JSON format
+    })
+    .catch((err) => { // If query was unsuccessful,
+        console.error(err); // Log error 
+        res.sendStatus(500); // And respond with 500 Internal Server Error
+    })
+})
+
+// DELETE method that deletes record with a specific ID
+app.delete("/presidents/:id", (req, res) => {
+    const id = parseInt(req.params.id); // Parse string into int and store in const 
+    if (isNaN(id)) { // Verify if it is a number 
+        res.status(400).send("Invalid ID"); // If not, send 400 message
+        return; // Terminate execution 
+    }
+
+    pool.query(`DELETE FROM presidents WHERE id = $1 RETURNING *`, [id]) // Query that deletes a record from the president table with a given id
+    .then((data) => { // If query was successful
+        if(data.rows.length === 0) { // Verify if record was deleted
+            res.status(404).send("No presidents found with that given id.");  // If not, respond with a 404 and message stating that no president had that Id
+        }
+        res.status(200).send(`President with id ${id} has been deleted.`); // If record was deleted, respond with a 200 and message stating it was successful
+    })
+    .catch((err) => { // If query was unsuccessful 
+        console.error(err); // Log error 
+        res.sendStatus(500); // Respond with a 500 Internal Server Error
+    })
+})
+
+// LISTEN method that listens on a given port for request
+app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`);
+});
+
